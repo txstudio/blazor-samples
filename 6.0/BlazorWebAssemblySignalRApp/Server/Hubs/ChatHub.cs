3caf906c453fe5385911a8203eddaf5df0fe34f4
William Wang <txstudio@outlook.com>
2022-11-24 18:21:48 +0800

    Add SignalR group sample

--- 6.0/BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
+++ 6.0/BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
@@ -2,11 +2,28 @@
 
 namespace BlazorWebAssemblySignalRApp.Server.Hubs
 {
+    public static class SignalRMethodName
+    {
+        public const string ReceiveAddGroupMessage = "ReceiveAddGroupMessage";
+        public const string ReceiveGroupMessage = "ReceiveGroupMessage";
+    }
+
     public class ChatHub : Hub
     {
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddGroup(string groupName, string userName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync(SignalRMethodName.ReceiveAddGroupMessage, $"{userName} add to group {groupName}");
+        }
+
+        public async Task SendMessageToGroup(string groupName, string userName, string message)
+        {
+            await Clients.Group(groupName).SendAsync(SignalRMethodName.ReceiveGroupMessage, groupName, userName, message);
         }
     }
 }
